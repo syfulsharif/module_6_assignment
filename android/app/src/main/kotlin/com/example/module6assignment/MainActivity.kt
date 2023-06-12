a8285dbddf8799b689584406b113994870a4ebef
Syful Sharif <sharif.syful@gmail.com>
2023-06-12 23:58:01 +0600

    Project Updated

---
+++ android/app/src/main/kotlin/com/example/module6assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module6assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: Text('Photo Gallery'),),
+    );
+  }
+}
+
+
+
